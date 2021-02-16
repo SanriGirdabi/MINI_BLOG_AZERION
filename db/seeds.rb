3beadf5bbceef2ee8d6e7f1e4ce4a6de00256329
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-02-16 17:15:20 +0300

    Add a seed and fix typo in comment model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+first_users = User.create([{ email: 'sercanuygur@gmail.com', password: '1234567' },
+                           { email: 'eyupsercanuygur@gmail.com', password: '8910111213' }])
+
+first_posts = Post.create([{ user_id: 1, header: 'Teknoloji', body: 'Günümüzde ilerleyen enerji ihtyacı...', color: 'red',
+                             title: 'Enerji İhtiyacı Öngörüleri', design: 'Bir kolon', date: DateTime.new(2021, 4, 2, 11) },
+                           { user_id: 2, header: 'Sanat', body: 'Postmodern sanatın eleştrisi konusu gündemden düşmeyen...',
+                            color: 'blue',
+                             title: 'Modern Sanat ve Eleştrisi', design: 'İki kolon', date: DateTime.new(2021, 5, 2, 15) }])
+
+first_comments = Comment.create({ user_id: 2, post_id: 1, date: DateTime.new(2021, 9, 2, 17),
+                                   body: 'Harika bir makale devamını bekliyoruz' })
