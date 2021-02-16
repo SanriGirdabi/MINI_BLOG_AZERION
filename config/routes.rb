91e5db1aa4f21015062889366c330c868b0f93f8
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-02-16 23:49:55 +0300

    Add devise views, change auth subdomain back

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/:id', to: 'users#show', constraints: { subdomain: 'users' }
-  get 'users/', to: 'users#index', constraints: { subdomain: 'users' }
   resources :comments, constraints: { subdomain: '' }
   resources :posts, constraints: { subdomain: '' }
-  devise_for :users, constraints: { subdomain: '' }
-  root 'posts#index', constraints: { subdomain: '' }
+  constraints subdomain: 'users' do
+    get 'users/:id', to: 'users#show'
+    get 'users/', to: 'users#index'
+  end
+  devise_for :users
+  root to: 'posts#index', constraints: { subdomain: '' }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
