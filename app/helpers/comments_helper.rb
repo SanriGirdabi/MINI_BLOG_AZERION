dae1e18a6816460ec5dc0c0bcb6d82589d8f071a
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-03-05 16:41:10 +0300

    Move the logic to helpers

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,8 @@
 module CommentsHelper
+  def error_checker?(variable)
+    @variable = variable
+    if variable.errors.any?
+      render 'error_section'
+    end
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,29 @@
 module PostsHelper
+  def can_comment?
+    if user_signed_in?
+      render 'post_comments_section'
+    end
+  end
+
+  def which_button?
+    if user_signed_in?
+      render 'sign_up_buttons'
+    else
+      render 'sign_in_buttons'
+    end
+  end
+
+  def authorized_buttons?(variable)
+    @variable = variable
+    if user_signed_in? && variable.user == current_user
+      render 'authorized_buttons'
+    end
+  end
+
+  def error_checker?(variable)
+    @variable = variable
+    if variable.errors.any?
+      render 'error_section'
+    end
+  end
 end
